d4f7ecba56d872eacc85d537b728572a5907d8e9
dev-matt <hgperu@gmail.com>
2021-12-07 23:36:56 -0500

    un cambio final

--- index.php
+++ index.php
@@ -5,4 +5,8 @@ ini_set('display_errors', '1');
 $file_handle = fopen('tmp.txt', 'r');
 $contents = fread($file_handle, filesize('tmp.txt'));
 fclose($file_handle);
+
+$myfile = fopen("tmp.txt", "r") or die("Unable to open file!");
+echo fread($myfile, filesize("tmp.txt"));
+fclose($myfile);
 ?>
