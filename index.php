007e24a786f4e1a5d63af3c2e990107b9fc3f283
dev-matt <hgperu@gmail.com>
2021-12-07 23:23:51 -0500

    mostrar errores en php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $file_handle = fopen('tmp.txt', 'r');
 $contents = fread($file_handle, filesize('tmp.txt'));
 fclose($file_handle);
