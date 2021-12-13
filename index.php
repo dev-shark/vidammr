145035f92800ba39cf56e3f558b1e04a5281ac9e
Adrian Devto <hgperu@gmail.com>
2021-12-13 02:39:02 -0500

    nuevos cambios del domingo

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+#error_reporting(E_ALL);
+#ini_set('display_errors', '1');
 
 $file_handle = fopen('tmp.txt', 'r');
 $contents = fread($file_handle, filesize('tmp.txt'));
 fclose($file_handle);
 
 $myfile = fopen("tmp.txt", "r") or die("Unable to open file!");
-echo fread($myfile, filesize("tmp.txt"));
+echo fread($myfile, filesize("tmp.txt"))."\n";
 fclose($myfile);
 
-function sum( $a , $b ){
-	$sum= $a + $b ;
-	return  $sum ;
+function suma($a,$b){
+	$sum= $a + $b;
+	return  $sum;
 }
 
-
-function res( $a , $b ){
-	$res= $a - $b ;
-	return  $res ;
+function resta($a,$b){
+	$res= $a - $b;
+	return  $res;
 }
 
-
-function pro( $a , $b ){
-	$pro= $a * $b ;
-	return  $pro ;
+function multiplicacion($a,$b){
+	$pro= $a * $b;
+	return  $pro;
 }
 
-echo sum(100,60);
-echo pro(5,5);
-echo res(100,87);
+function division($a,$b){
+  $div= $a / $b;
+  return $div;
+}
+
+echo suma(100,60)."\n";
+echo resta(15,5)."\n";
+echo multiplicacion(100,87)."\n";
+echo division(100,25)."\n";
 ?>
